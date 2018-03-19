ef521de6e7bec226a93197ba0a8be9ba710d1d97
lydia <lydia_andrews18@yahoo.com>
2018-03-19 01:23:12 -0400

    updated properties file

--- src/main/java/com/lydia/employeedetail/service/StartUp.java
+++ src/main/java/com/lydia/employeedetail/service/StartUp.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,13 @@ public class StartUp implements  ApplicationRunner {
 	@Autowired
 	MongoService mongoService;
 	private static final Logger LOGGER= LoggerFactory.getLogger(StartUp.class);
-
+	@Value("${app.mongo.source.data}")
+	private String dataFilePath;
 	@Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
 		String jsonData;
 		try {
-			jsonData = Utility.readFile("src/main/resources/employeedata.Json",StandardCharsets.UTF_8);
+			jsonData = Utility.readFile(dataFilePath,StandardCharsets.UTF_8);
 			EmployeeData employeeData = new Gson().fromJson(jsonData,EmployeeData.class);
 			mongoService.bulkInsert(employeeData.getEmployeeData(), "employeeDetails");
 			LOGGER.info("Data Refreshed in mongo DB");
